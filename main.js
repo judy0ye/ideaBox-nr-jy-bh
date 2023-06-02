15096353ce7d332fbdbd16d4835659168af51534
Judy Ye <judyy.ye@gmail.com>
2023-06-02 12:25:51 -0700

    fix: Resolve js merge conflict

--- main.js
+++ main.js
@@ -16,6 +16,16 @@ cardGrid.addEventListener("click", function (e) {
   deleteMiniCard(e);
 });
 
+saveButton.addEventListener('mouseover', function(){
+    if(!saveButton.disabled){
+        saveButton.classList.add('hover');
+    }
+})
+
+saveButton.addEventListener('mouseout', function(){
+    saveButton.classList.remove('hover');
+})
+
 // functions
 function createIdea(title, body) {
   return {
@@ -39,11 +49,38 @@ function makeNewIdea(e) {
     ) {
       return;
     }
-  }
-  ideas.push(currentIdea);
-  clearInputs();
-  showCards();
+    ideas.push(currentIdea)
+    clearInputs();
+    saveButton.disabled = true;
+    showCards();  
+    }
 }
+
+function showCards(){
+    cardGrid.innerHTML = '';
+    for(var i = 0; i < ideas.length; i++){
+        cardGrid.innerHTML += `
+        <article class="mini-card" id="${ideas[i].id}">
+            <h2> ${ideas[i].title} </h2>
+            <p> ${ideas[i].body}</p>
+            </article> `
+    }
+}
+
+function clearInputs(){
+    titleInput.value = '';
+    bodyInput.value= '';
+}
+
+function checkInput(){
+    if(titleInput.value.trim() !== '' && bodyInput.value.trim() !== ''){
+        saveButton.disabled = false;
+    }
+    else{
+        saveButton.disabled = true;
+    }
+}
+
 
 function showCards() {
   cardGrid.innerHTML = "";
@@ -63,8 +100,6 @@ function clearInputs() {
   titleInput.value = "";
   bodyInput.value = "";
 }
-
-
 
 function deleteMiniCard(e) {
   for (var i = 0; i < ideas.length; i++) {
