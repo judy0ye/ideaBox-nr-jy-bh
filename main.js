b88b6a8a63c07bcb489e3301b8f73d47f9157cc7
Judy Ye <judyy.ye@gmail.com>
2023-06-02 00:15:48 -0700

    fix: Refactor deleteMiniCard function to not show type error on console upon card-grid click

--- main.js
+++ main.js
@@ -49,11 +49,13 @@ function showCards(){
     for(var i = 0; i < ideas.length; i++){
         cardGrid.innerHTML += `
         <article class="mini-card" id="${ideas[i].id}">
-            <header id="mini-card-header"><button id="delete-button"></button></header>
+            <header id="mini-card-header">
+                <button class="delete-button"></button>
+            </header>
             
             <h2> ${ideas[i].title} </h2>
             <p> ${ideas[i].body}</p>
-            </article> `
+        </article> `
     }
 }
 
@@ -62,15 +64,25 @@ function clearInputs(){
     bodyInput.value= '';
 }
 
+// function deleteMiniCard(e) {
+//     for (var i = 0; i < ideas.length; i++) {
+//         console.log('potato')
+//         if (parseInt(e.target.closest('article').id) === ideas[i].id) {
+//             ideas.splice(i, 1)
+//         }
+//     }
+//     showCards()
+// }
+
 function deleteMiniCard(e) {
     for (var i = 0; i < ideas.length; i++) {
-        console.log('potato')
-        if (parseInt(e.target.closest('article').id) === ideas[i].id) {
-            ideas.splice(i, 1)
+        console.log(e.target.parentElement)
+        if (e.target.classList.contains('delete-button')) {
+           cardGrid.removeChild(e.target.parentElement.parentElement)
+           && ideas.splice(i,1) 
         }
     }
-    showCards()
+    //showCards()
 }
-
 {/* <div><button id="delete-button"></button></div> */}
 {/* <header id="mini-card-header"><div class="svg"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18.18 17.72"><defs><style>.cls-1{fill:#fff;}</style></defs><title>Artboard 31</title><g id="background"><polygon class="cls-1" points="13.13 5.87 12.43 5.16 8.97 8.61 5.53 5.16 4.82 5.87 8.27 9.32 4.82 12.77 5.53 13.48 8.97 10.03 12.43 13.48 13.13 12.77 9.68 9.32 13.13 5.87"/></g></svg></div></header> */}
